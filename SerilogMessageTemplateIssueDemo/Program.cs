f69d2a5eb759d09739c218879d67754d8438ed3e
Alex Koshelev <alexanderk@pressreader.com>
2015-11-14 18:56:02 -0800

    Initial commit

---
+++ SerilogMessageTemplateIssueDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Akka.Configuration;
+using Serilog;
+
+namespace SerilogMessageTemplateIssueDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+              .MinimumLevel.Debug()
+              .WriteTo.ColoredConsole(outputTemplate: "[{Level}][{LogSource}][Thread {Thread}] {Message}{NewLine}{Exception}")
+              .CreateLogger();
+
+            using (
+                var actorSystem = ActorSystem.Create("test",
+                    ConfigurationFactory.ParseString("akka {  loggers = [\"Akka.Logger.Serilog.SerilogLogger, Akka.Logger.Serilog\"] } "))) 
+            {
+                actorSystem.ActorOf(Props.Create(() => new SimpleActor()), "simple");
+                actorSystem.Shutdown();
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+
+}
---
+++ SerilogMessageTemplateIssueDemo/SimpleActor.cs
@@ -0,0 +1,18 @@
+﻿using Akka.Actor;
+using Akka.Event;
+
+namespace SerilogMessageTemplateIssueDemo
+{
+    public class SimpleActor : ReceiveActor
+    {
+        private ILoggingAdapter _logger = Context.GetLogger();
+
+        public SimpleActor()
+        {
+            // output will be [Information][{LogSource}][Thread {Thread}] "Actor is up and running."
+            // while expected [Information][<actor path>][Thread <thread id>] "Actor is up and running."
+            _logger.Info("Actor is up and running.");
+        }
+
+    }
+}
